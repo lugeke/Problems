cb2ce5f0c6168c19e48218e82b4486a37deb93fc
jiaheng <lugeke@gmail.com>
2021-03-06 17:04:18 +0800

    add L215 Kth Largest Element In An Array

---
+++ leetcode/src/L215KthLargestElementInAnArray.java
@@ -0,0 +1,39 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class L215KthLargestElementInAnArray {
+
+    public static int findKthLargest(int[] nums, int k) {
+
+        return kthLargest(nums, 0, nums.length - 1, k - 1);
+    }
+
+    private static int kthLargest(int[] nums, int start, int end, int k) {
+        // start 到 end 之间选取一个随机数
+        int random = ThreadLocalRandom.current().nextInt(start, end + 1);
+        swap(nums, start, random);
+        int pivot = nums[random];
+        // 将比pivot大的放到 pivot 的左边
+        // invariant
+        // nums[start+1 ... index]    > pivot
+
+        int index = start;
+        for (int i = start + 1; i <= end; i++) {
+            if (nums[i] > pivot) {
+                swap(nums, i, ++index);
+            }
+        }
+        swap(nums, index, start);
+
+        int ith = index - start;
+        if (ith == k) return pivot;
+        else if (k < ith) return kthLargest(nums, start, index - 1, k);
+        else return kthLargest(nums, index + 1, end, k - (ith + 1));
+    }
+
+    private static void swap(int[] ary, int i, int j) {
+        if (i == j) return;
+        int t = ary[i];
+        ary[i] = ary[j];
+        ary[j] = t;
+    }
+}
