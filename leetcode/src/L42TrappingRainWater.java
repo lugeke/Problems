91a4d54645c016aa9b668f9029d7529e9ae50f65
jiaheng <lugeke@gmail.com>
2021-03-17 21:47:01 +0800

    添加双指针解法

--- leetcode/src/L42TrappingRainWater.java
+++ leetcode/src/L42TrappingRainWater.java
@@ -15,10 +15,40 @@ public class L42TrappingRainWater {
         }
 
         int sum = 0;
+
+        // i能装的水量为 左边的最大值和右边的最大值中较小的一个 与 自己的差
         for (int i = 1; i < height.length - 1; i++) {
             sum += Math.max(Math.min(leftMax[i], rightMax[i]) - height[i], 0);
         }
         return sum;
+    }
+
+
+    // 先找到最高的， 然后从两头往中间遍历
+    public static int trap1(int[] height) {
+        if (height == null || height.length < 3) return 0;
+        int maxIndex = 0;
+        for (int i = 1; i < height.length; i++) {
+            if (height[i] > height[maxIndex])
+                maxIndex = i;
+        }
+
+        int water = 0;
+        int peak = 0;
+        for (int i = 0; i < maxIndex; i++) {
+            int h = height[i];
+            if (h > peak) peak = h;
+            else water += peak - h;
+        }
+
+        peak = 0;
+        for (int i = height.length - 1; i > maxIndex; i--) {
+            int h = height[i];
+            if (h > peak) peak = h;
+            else water += peak - h;
+        }
+
+        return water;
     }
 
 
--- leetcode/test/L42TrappingRainWaterTest.java
+++ leetcode/test/L42TrappingRainWaterTest.java
@@ -8,11 +8,13 @@ public class L42TrappingRainWaterTest {
     public void trap1() {
         int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
         assertEquals(6, L42TrappingRainWater.trap(height));
+        assertEquals(6, L42TrappingRainWater.trap1(height));
     }
 
     @Test
     public void trap2() {
         int[] height = {4, 2, 0, 3, 2, 5};
         assertEquals(9, L42TrappingRainWater.trap(height));
+        assertEquals(9, L42TrappingRainWater.trap1(height));
     }
 }
