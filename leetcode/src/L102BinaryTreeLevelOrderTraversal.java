04a12afb755ba86edc7c217c962b8bebea1a45be
jiaheng <lugeke@gmail.com>
2021-03-17 15:08:29 +0800

    精简代码

--- leetcode/src/L102BinaryTreeLevelOrderTraversal.java
+++ leetcode/src/L102BinaryTreeLevelOrderTraversal.java
@@ -13,27 +13,18 @@ public class L102BinaryTreeLevelOrderTraversal {
 
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
-        int levelCount = 1;
-        while (levelCount > 0) {
-            int nextCount = 0;
+        while (!queue.isEmpty()) {
+            int count = queue.size();
             List<Integer> level = new ArrayList<>();
-            for (int i = 0; i < levelCount; i++) {
-                TreeNode n = queue.poll();
+            for (int i = 0; i < count; i++) {
+                TreeNode n = queue.remove();
                 level.add(n.val);
-                if (n.left != null) {
-                    queue.offer(n.left);
-                    nextCount++;
-                }
-                if (n.right != null) {
-                    queue.offer(n.right);
-                    nextCount++;
-                }
+                if (n.left != null) queue.offer(n.left);
+                if (n.right != null) queue.offer(n.right);
             }
 
             levels.add(level);
-            levelCount = nextCount;
         }
-
         return levels;
     }
 
