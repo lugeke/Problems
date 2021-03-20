5379fba2954e11626c1d7c69c13cb6d3cc7a5af1
jiaheng <lugeke@gmail.com>
2021-03-20 22:02:09 +0800

    更简洁的写法

--- leetcode/src/L56MergeIntervals.java
+++ leetcode/src/L56MergeIntervals.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.LinkedList;
 
 public class L56MergeIntervals {
 
@@ -25,6 +26,26 @@ public class L56MergeIntervals {
         if (!Arrays.equals(i, result[count == 0 ? 0 : count - 1])) result[count++] = i;
 
         return Arrays.copyOfRange(result, 0, count);
+    }
+
+
+    // 更简洁的写法
+    public static int[][] merge1(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        LinkedList<int[]> merged = new LinkedList<>();
+        for (int[] interval : intervals) {
+            // if the list of merged intervals is empty or if the current
+            // interval does not overlap with the previous, simply append it.
+            if (merged.isEmpty() || merged.getLast()[1] < interval[0]) {
+                merged.add(interval);
+            }
+            // otherwise, there is overlap, so we merge the current and previous
+            // intervals.
+            else {
+                merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
     }
 
 
