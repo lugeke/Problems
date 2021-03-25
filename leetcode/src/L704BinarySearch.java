e9a5d11542da763b054d759540963137b36e297d
jiaheng <lugeke@gmail.com>
2021-03-25 09:05:02 +0800

    add L704 BinarySearch

---
+++ leetcode/src/L704BinarySearch.java
@@ -0,0 +1,17 @@
+public class L704BinarySearch {
+
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high-low) / 2;
+
+            if (target < nums[mid]) high = mid - 1;
+            else if ( target > nums[mid]) low = mid + 1;
+            else return mid;
+        }
+
+        return -1;
+    }
+
+
+}
