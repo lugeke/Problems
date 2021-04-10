6ace67e797f371662307ee96d9a16442dc9b5f3e
jiaheng <lugeke@gmail.com>
2021-04-10 14:53:30 +0800

    add O21 Exchange

---
+++ leetcode/src/O21Exchange.java
@@ -0,0 +1,18 @@
+import static ds.Utils.swap;
+
+public class O21Exchange {
+
+    public int[] exchange(int[] nums) {
+        int index = 0;
+        // 0..<index 是奇数
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 1) {
+                swap(nums, i, index++);
+            }
+        }
+
+        return nums;
+    }
+
+
+}
